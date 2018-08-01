72502c08648cd3b7312b7aca38f19a765467dc26
zzd <zhangzdhere@gmail.com>
2018-08-01 17:25:58 +0800

    first_vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '模板首页！'
+    }
+  }
+}
+</script>
