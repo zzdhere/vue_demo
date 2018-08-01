440e6352ccc1079c6ac199df615e0e9c414bdf76
zzd <zhangzdhere@gmail.com>
2018-08-01 17:37:25 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,23 @@
 </template>
 
 <script>
+
+import Hello from './components/Hello'
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: '模板首页！'
-    }
+  name: 'App',
+  components:{
+    Hello
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
